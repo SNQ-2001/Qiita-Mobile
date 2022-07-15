20f03a4dfc81dad381be6c8dc7372070c50903cc
SNQ-2001 <taishin05109@gmail.com>
2022-07-15 11:27:59 +0900

    更新: HomeViewControllerをHomeViewModelに分割

--- Qiita/Sources/HomeViewController.swift
+++ Qiita/Sources/HomeViewController.swift
@@ -6,22 +6,10 @@
 //
 
 import UIKit
-import Alamofire
-import SwiftSoup
-import KeychainAccess
-import SwiftUI
 
 class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    let keychain = Keychain(service: "com.Qiita")
-
-    var csrfToken: String = ""
-
-    var homeIndexPage: HomeIndexPage = HomeIndexPage(personalizedFeed: HomeIndexPagePersonalizedFeed(personalizedFeed: PersonalizedFeedPersonalizedFeed(edges: []))) {
-        didSet {
-            tableView.reloadData()
-        }
-    }
+    let homeViewModel = HomeViewModel()
 
     @IBOutlet var tableView: UITableView!
 
@@ -33,16 +21,18 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
         tableView.separatorInset = .zero
 
-        getCsrfToken()
+        initViewModel()
+
+        homeViewModel.requestHomeIndexPage()
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return homeIndexPage.personalizedFeed.personalizedFeed.edges.count
+        return homeViewModel.homeIndexPage.personalizedFeed.personalizedFeed.edges.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell") as? HomeTableViewCell {
-            let node = homeIndexPage.personalizedFeed.personalizedFeed.edges[indexPath.row].node
+            let node = homeViewModel.homeIndexPage.personalizedFeed.personalizedFeed.edges[indexPath.row].node
             cell.setCell(
                 profileImageURL: node.author.profileImageURL,
                 title: node.title,
@@ -56,60 +46,14 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return UITableViewCell()
     }
 
-    func getCsrfToken() {
-        guard let user_session_key = keychain["user_session_key"] else { return }
-        guard let secure_token = keychain["secure_token"] else { return }
-        guard let _qiita_login_session = keychain["_qiita_login_session"] else { return }
-        let headers: HTTPHeaders? = [
-            "cookie": "user_session_key=\(user_session_key); secure_token=\(secure_token); _qiita_login_session=\(_qiita_login_session)",
-            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
-        ]
-        AF.request("https://qiita.com/", method: .get, headers: headers).responseData { response in
-            guard let data = response.data else { return }
-            do {
-                let html: String = String(data: data, encoding: .utf8) ?? ""
-                let doc: Document = try SwiftSoup.parse(html)
-//                let link: Element = try doc.getElementById("js-react-on-rails-context")!
-//                let csrf_token = try JSONDecoder().decode(CSRFToken.self, from: link.data().data(using: .utf8)!)
-//
-//                self.csrfToken = csrf_token.settings.csrfToken
-
-                let component: Elements = try doc.getElementsByClass("js-react-on-rails-component")
-                let elements: [Element] = component.array()
-                for element in elements {
-                    if try element.attr("data-component-name") == "HomeIndexPage" {
-                        let json = try JSONDecoder().decode(HomeIndexPage.self, from: element.data().data(using: .utf8)!)
-                        self.homeIndexPage = json
-                    }
-                }
-
-            } catch Exception.Error(_, let message) {
-                print(message)
-            } catch {
-                print("error")
+    func initViewModel() {
+        self.homeViewModel.reloadHandler = { [weak self] in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
             }
         }
     }
-
-    // 更新
-    func getJSON() {
-        guard let user_session_key = keychain["user_session_key"] else { return }
-        guard let secure_token = keychain["secure_token"] else { return }
-        guard let _qiita_login_session = keychain["_qiita_login_session"] else { return }
-
-        let headers: HTTPHeaders? = [
-            "cookie": "user_session_key=\(user_session_key); secure_token=\(secure_token); _qiita_login_session=\(_qiita_login_session)",
-            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
-            "x-csrf-token": "\(csrfToken)"
-        ]
-
-        let parameters: [String: Any] = ["operationName": "GetPersonalizedFeed", "variables": ["after": "MzA=", "first": 30], "query": "query GetPersonalizedFeed($first: Int!, $after: String) {\n  personalizedFeed(first: $first, after: $after) {\n    ...PersonalizedFeedResult\n    __typename\n  }\n}\n\nfragment PersonalizedFeedResult on FeedConnection {\n  edges {\n    isLikedByViewer\n    followingLikers {\n      profileImageUrl\n      urlName\n      __typename\n    }\n    node {\n      ...UnifiedArticleResult\n      __typename\n    }\n    __typename\n  }\n  pageInfo {\n    endCursor\n    hasNextPage\n    __typename\n  }\n  __typename\n}\n\nfragment UnifiedArticleResult on Article {\n  encryptedId\n  isLikedByViewer\n  isStockableByViewer\n  isStockedByViewer\n  likesCount\n  linkUrl\n  publishedAt\n  title\n  uuid\n  author {\n    profileImageUrl\n    urlName\n    name\n    __typename\n  }\n  organization {\n    name\n    urlName\n    __typename\n  }\n  followingLikers {\n    urlName\n    __typename\n  }\n  recentlyFollowingLikers {\n    profileImageUrl\n    urlName\n    __typename\n  }\n  tags {\n    urlName\n    name\n    __typename\n  }\n  __typename\n}\n"]
-        AF.request("https://qiita.com/graphql", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseData { response in
-            guard let data = response.data else { return }
-
-            print(String.init(data: data, encoding: .utf8)!)
-        }
-    }
+    
 }
 
 
--- Qiita/Sources/ViewModel/HomeViewModel.swift
+++ Qiita/Sources/ViewModel/HomeViewModel.swift
@@ -6,16 +6,80 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftSoup
+import KeychainAccess
 
-class ViewModel: NSObject {
-    var inputText: String?
+class HomeViewModel: NSObject {
 
-    func greetings() -> String {
-        let name = inputText?.uppercased() ?? ""
-        return "Hello, \(name)!"
+    /// キーチェーン
+    public let keychain = Keychain(service: "com.Qiita")
+
+    /// リロードハンドラー
+    /// アクションをViewControllerから受け取る
+    public var reloadHandler: () -> Void = { }
+
+
+    /// リポジトリ情報の格納庫
+    /// 変更が検知されるとリロードハンドラーが実行される
+    public var homeIndexPage: HomeIndexPage = HomeIndexPage(personalizedFeed: HomeIndexPagePersonalizedFeed(personalizedFeed: PersonalizedFeedPersonalizedFeed(edges: []))) {
+        didSet {
+            reloadHandler()
+        }
     }
 
-    func buttonIsEnabled() -> Bool {
-        return !(inputText ?? "").isEmpty
+    public func requestHomeIndexPage() {
+        guard let user_session_key = self.keychain["user_session_key"] else { return }
+        guard let secure_token = self.keychain["secure_token"] else { return }
+        guard let _qiita_login_session = self.keychain["_qiita_login_session"] else { return }
+        let headers: HTTPHeaders? = [
+            "cookie": "user_session_key=\(user_session_key); secure_token=\(secure_token); _qiita_login_session=\(_qiita_login_session)",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
+        ]
+        AF.request("https://qiita.com/", method: .get, headers: headers).responseData { response in
+            guard let data = response.data else { return }
+            do {
+                let html: String = String(data: data, encoding: .utf8) ?? ""
+                let doc: Document = try SwiftSoup.parse(html)
+//                let link: Element = try doc.getElementById("js-react-on-rails-context")!
+//                let csrf_token = try JSONDecoder().decode(CSRFToken.self, from: link.data().data(using: .utf8)!)
+//
+//                self.csrfToken = csrf_token.settings.csrfToken
+
+                let component: Elements = try doc.getElementsByClass("js-react-on-rails-component")
+                let elements: [Element] = component.array()
+                for element in elements {
+                    if try element.attr("data-component-name") == "HomeIndexPage" {
+                        let json = try JSONDecoder().decode(HomeIndexPage.self, from: element.data().data(using: .utf8)!)
+                        self.homeIndexPage = json
+                    }
+                }
+
+            } catch Exception.Error(_, let message) {
+                print(message)
+            } catch {
+                print("error")
+            }
+        }
     }
+
+    // 更新
+//    func requestHomeIndexPageReload() {
+//        guard let user_session_key = keychain["user_session_key"] else { return }
+//        guard let secure_token = keychain["secure_token"] else { return }
+//        guard let _qiita_login_session = keychain["_qiita_login_session"] else { return }
+//
+//        let headers: HTTPHeaders? = [
+//            "cookie": "user_session_key=\(user_session_key); secure_token=\(secure_token); _qiita_login_session=\(_qiita_login_session)",
+//            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
+//            "x-csrf-token": "\(csrfToken)"
+//        ]
+//
+//        let parameters: [String: Any] = ["operationName": "GetPersonalizedFeed", "variables": ["after": "MzA=", "first": 30], "query": "query GetPersonalizedFeed($first: Int!, $after: String) {\n  personalizedFeed(first: $first, after: $after) {\n    ...PersonalizedFeedResult\n    __typename\n  }\n}\n\nfragment PersonalizedFeedResult on FeedConnection {\n  edges {\n    isLikedByViewer\n    followingLikers {\n      profileImageUrl\n      urlName\n      __typename\n    }\n    node {\n      ...UnifiedArticleResult\n      __typename\n    }\n    __typename\n  }\n  pageInfo {\n    endCursor\n    hasNextPage\n    __typename\n  }\n  __typename\n}\n\nfragment UnifiedArticleResult on Article {\n  encryptedId\n  isLikedByViewer\n  isStockableByViewer\n  isStockedByViewer\n  likesCount\n  linkUrl\n  publishedAt\n  title\n  uuid\n  author {\n    profileImageUrl\n    urlName\n    name\n    __typename\n  }\n  organization {\n    name\n    urlName\n    __typename\n  }\n  followingLikers {\n    urlName\n    __typename\n  }\n  recentlyFollowingLikers {\n    profileImageUrl\n    urlName\n    __typename\n  }\n  tags {\n    urlName\n    name\n    __typename\n  }\n  __typename\n}\n"]
+//        AF.request("https://qiita.com/graphql", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseData { response in
+//            guard let data = response.data else { return }
+//
+//            print(String.init(data: data, encoding: .utf8)!)
+//        }
+//    }
 }
