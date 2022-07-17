f0ed3f462626f26578a4d7633df834c6c854b1bb
SNQ-2001 <taishin05109@gmail.com>
2022-07-17 15:54:47 +0900

    更新: Viewファイルを作成

---
+++ Qiita/Sources/View/TimelineViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TimelineViewController.swift
+//  Qiita
+//
+//  Created by 宮本大新 on 2022/07/09.
+//
+
+import UIKit
+import KeychainAccess
+
+class TimelineViewController: UIViewController {
+    let keychain = Keychain(service: "com.Qiita")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "タイムライン"
+
+//        keychain["_qiita_login_session"] = nil
+//        keychain["_ga"] = nil
+//        keychain["_gid"] = nil
+//        keychain["__gads"] = nil
+//        keychain["__gpi"] = nil
+//        keychain["mp_17d24b448ca579c365d2d1057f3a1791_mixpanel"] = nil
+//        keychain["user_session_key"] = nil
+//        keychain["secure_token"] = nil
+//        keychain["_td"] = nil
+//        keychain["general_banner_displayed"] = nil
+
+
+        
+    }
+
+}
